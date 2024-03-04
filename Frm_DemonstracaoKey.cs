89f940fd9bf23cb36f1083bd3a684f41d9667b89
Murilo Coelho <m.coelhos1212@gmail.com>
2024-03-03 23:00:11 -0300

    feat: create new form and capturing keys pressed by the user

---
+++ Frm_DemonstracaoKey.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Frm_DemonstracaoKey : Form
+    {
+        public Frm_DemonstracaoKey()
+        {
+            InitializeComponent();
+        }
+
+        private void Txt_Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            Txt_Msg.AppendText("\r\n" + "Pressionei a tecla " + e.KeyCode + "\r\n");
+            Txt_Msg.AppendText("\r\n" + "Código da tecla " + ((int)e.KeyCode) + "\r\n");
+            Txt_Msg.AppendText("\r\n" + "Nome da tecla " + e.KeyData + "\r\n");
+            Lbl_Lower.Text = e.KeyCode.ToString().ToLower();
+            Lbl_Upper.Text = e.KeyCode.ToString().ToUpper();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Txt_Msg.Text = null;
+            Txt_Input.Text = null;
+            Lbl_Upper.Text = null;
+            Lbl_Lower.Text = null;
+        }
+    }
+}
